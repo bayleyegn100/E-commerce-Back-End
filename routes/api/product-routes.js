e3912179a870e7c87b7d24c47a7f07a4a86c584b
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-09 00:38:42 -0700

    for getttin all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,15 +3,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
+// find all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category, as: 'category' }, { model: Tag, through: ProductTag }]
+    });
+    res.json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// get one product
+ // find a single product by its `id`
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
+ 
   // be sure to include its associated Category and Tag data
 });
 
