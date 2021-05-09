e4fe82409796293b393264aec2123e1a2292ec03
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-09 01:12:29 -0700

    For Products beloges to many Tags and visevers

--- models/index.js
+++ models/index.js
@@ -4,14 +4,22 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  through: {
+    model: ProductTag,
+    foreignKey: "product_id",
+    unique: false
+  }
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product,{
+  through: {
+    model: ProductTag,
+    foreignKey: "tag_id",
+    unique: false
+  }
+})
 module.exports = {
   Product,
   Category,
