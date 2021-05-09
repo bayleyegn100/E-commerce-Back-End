77f70ed5ad9bc0c20c80c566777f7b78a612047d
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-09 00:40:16 -0700

    finding a single producte by using its ID

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // find all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
       include: [{ model: Category, as: 'category' }, { model: Tag, through: ProductTag }]
@@ -16,9 +16,21 @@ router.get('/', (req, res) => {
 });
 
  // find a single product by its `id`
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
  
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, as: 'category' }, { model: Tag, through: ProductTag }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this ID!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
