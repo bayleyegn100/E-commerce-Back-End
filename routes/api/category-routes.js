95ebd641ca5842e9cfb11d8eae1536643b8db81c
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-09 00:13:46 -0700

    Posting a new catagory

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,9 +31,14 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-router.post('/', (req, res) => {
-  // create a new category
+ // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
